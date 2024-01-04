252f106ad85c836da36b49795cb24e9a63e8492f
Michael Goppert <goppert@utexas.edu>
2024-01-03 18:19:11 -0600

    Full lookup support

--- bin/index.js
+++ bin/index.js
@@ -2,41 +2,212 @@
 const yargs = require('yargs');
 const usage = '\nUsage: eri <flags> <args>';
 const options = yargs
-	.usage(usage)
-	.option('h', {
-		alias: 'help',
-		describe: 'Show help',
-		type: 'boolean',
-		demandOption: false,
-	})
-	.option('about', {
-		describe: 'About eri',
-		type: 'boolean',
-		demandOption: false,
-	})
-	.option('omen', {
-		type: 'boolean',
-		demandOption: false,
-	})
-	.option('i', {
-		alias: 'item',
-		describe: 'Item inquiry',
-		type: 'string',
-		demandOption: false,
-	})
-	.help(true).argv;
+    .usage(usage)
+    .option('h', {
+        alias: 'help',
+        describe: 'Show help',
+        type: 'boolean',
+        demandOption: false,
+    })
+    .option('about', {
+        describe: 'About eri',
+        type: 'boolean',
+        demandOption: false,
+    })
+    .option('omen', {
+        type: 'boolean',
+        demandOption: false,
+    })
+    .option('ammo', {
+        describe: 'Ammo inquiry',
+        type: 'string',
+        demandOption: false,
+    })
+    .option('armor', {
+        describe: 'Armor inquiry',
+        type: 'string',
+        demandOption: false,
+    })
+    .option('ashe', {
+        describe: 'Ashes of War inquiry',
+        type: 'string',
+        demandOption: false,
+    })
+    .option('boss', {
+        describe: 'Boss inquiry',
+        type: 'string',
+        demandOption: false,
+    })
+    .option('class', {
+        describe: 'Class inquiry',
+        type: 'string',
+        demandOption: false,
+    })
+    .option('creature', {
+        describe: 'Creature inquiry',
+        type: 'string',
+        demandOption: false,
+    })
+    .option('incantation', {
+        describe: 'Incantation inquiry',
+        type: 'string',
+        demandOption: false,
+    })
+    .option('item', {
+        describe: 'Item inquiry',
+        type: 'string',
+        demandOption: false,
+    })
+    .option('location', {
+        describe: 'Location inquiry',
+        type: 'string',
+        demandOption: false,
+    })
+    .option('npc', {
+        describe: 'NPC inquiry',
+        type: 'string',
+        demandOption: false,
+    })
+    .option('shield', {
+        describe: 'Shield inquiry',
+        type: 'string',
+        demandOption: false,
+    })
+    .option('sorcery', {
+        describe: 'Sorcery inquiry',
+        type: 'string',
+        demandOption: false,
+    })
+    .option('spirit', {
+        describe: 'Spirit inquiry',
+        type: 'string',
+        demandOption: false,
+    })
+    .option('talisman', {
+        describe: 'Talisman inquiry',
+        type: 'string',
+        demandOption: false,
+    })
+    .option('weapon', {
+        describe: 'Weapon inquiry',
+        type: 'string',
+        demandOption: false,
+    })
+    .help(true).argv;
 const art = require('./art.js');
 const process = require('./process.js');
 
 if (yargs.argv.about) {
-	art.drawAbout();
+    art.drawAbout();
 }
 
 if (yargs.argv.omen) {
-	art.drawOmen();
+    art.drawOmen();
 }
 
-if (yargs.argv.i || yargs.argv.item) {
-	var item = yargs.argv.i || yargs.argv.item;
-	process.processQuery(item, 'items');
+if (yargs.argv.ammo) {
+    process.processQuery(yargs.argv.ammo, 'ammos').then((res) => process.printData(
+        res,
+        new Set(['name', 'description', 'type', 'passive', 'attackPower'])
+    ));
+}
+
+if (yargs.argv.armor) {
+    process.processQuery(yargs.argv.armor, 'armor').then((res) => process.printData(
+        res,
+        new Set(['name', 'description', 'category', 'weight', 'dmgNegation', 'resistance'])
+    ));
+}
+
+if (yargs.argv.ashe) {
+    process.processQuery(yargs.argv.ashe, 'ashes').then((res) => process.printData(
+        res,
+        new Set(['name', 'description', 'affinity', 'skill'])
+    ));
+}
+
+if (yargs.argv.boss) {
+    process.processQuery(yargs.argv.boss, 'bosses').then((res) => process.printData(
+        res,
+        new Set(['name', 'description', 'location', 'drops', 'healthPoints'])
+    ));
+}
+
+if (yargs.argv.class) {
+    process.processQuery(yargs.argv.class, 'classes').then((res) => process.printData(
+        res,
+        new Set(['name', 'description', 'stats'])
+    ));
+}
+
+if (yargs.argv.creature) {
+    process.processQuery(yargs.argv.creature, 'creatures').then((res) => process.printData(
+        res,
+        new Set(['name', 'description', 'location', 'drops'])
+    ));
+}
+
+if (yargs.argv.incantation) {
+    process.processQuery(yargs.argv.incantation, 'incantations').then((res) => process.printData(
+        res,
+        new Set(['name', 'description', 'type', 'cost', 'slots', 'effects', 'requires'])
+    ));
+}
+
+if (yargs.argv.item) {
+    process.processQuery(yargs.argv.item, 'items').then((res) => process.printData(
+        res,
+        new Set(['name', 'description', 'type', 'effect'])
+    ));
+}
+
+if (yargs.argv.location) {
+    process.processQuery(yargs.argv.location, 'locations').then((res) => process.printData(
+        res,
+        new Set(['name', 'description'])
+    ));
+}
+
+if (yargs.argv.npc) {
+    process.processQuery(yargs.argv.npc, 'npcs').then((res) => process.printData(
+        res,
+        new Set(['name', 'description', 'location', 'quote'])
+    ));
+}
+
+if (yargs.argv.shield) {
+    process.processQuery(yargs.argv.shield, 'shields').then((res) => process.printData(
+        res,
+        new Set(['name', 'description', 'category', 'weight', 'attack', 'defence', 'requiredAttributes', 'scalesWith'])
+    ));
+}
+
+if (yargs.argv.sorcery) {
+    process.processQuery(yargs.argv.sorcery, 'sorceries').then((res) => process.printData(
+        res,
+        new Set(['name', 'description', 'type', 'cost', 'slots', 'effects', 'requires'])
+    ));
+}
+
+if (yargs.argv.spirit) {
+    process.processQuery(yargs.argv.spirit, 'spirits').then((res) => process.printData(
+        res,
+        new Set(['name', 'description', 'fpCost', 'hpCost', 'effects'])
+    ));
+}
+
+if (yargs.argv.talisman) {
+    process.processQuery(yargs.argv.talisman, 'talismans').then((res) => process.printData(
+        res,
+        new Set(['name', 'description', 'effects'])
+    ));
+}
+
+if (yargs.argv.weapon) {
+    process
+        .processQuery(yargs.argv.weapon, 'weapons')
+        .then((res) => process.printData(
+            res,
+            new Set(['name', 'description', 'category', 'weight', 'attack', 'defence', 'requiredAttributes', 'scalesWith'])
+        ));
 }
--- bin/process.js
+++ bin/process.js
@@ -1,26 +1,112 @@
 module.exports = {
-	processQuery: processQuery,
+    processQuery: processQuery,
+    printData: printData,
 };
+
+const chalk = require('chalk');
+
+const name = chalk.bold.underline;
+const quote = chalk.italic;
+const attrName = chalk.bold.red;
 
 const erapi = 'https://eldenring.fanapis.com/api/';
 
-function processQuery(name, type) {
-	grabInfo(erapi + `${type}?name=` + encodeURIComponent(name)).then(
-		(info) => {
-			console.log(info);
-		}
-	);
+async function processQuery(name, type) {
+    let url = `${erapi}${type}?name=${encodeURIComponent(name)}`;
+    let info = await grabInfo(url);
+    return info;
 }
 
-function grabInfo(url) {
-	return fetch(url)
-		.then((response) => {
-			if (!response.ok) {
-				throw new Error(`E01: Failed to fetch ${url}`);
-			}
-			return response.json();
-		})
-		.catch((error) => {
-			console.error('E02: ', error);
-		});
+async function grabInfo(url) {
+    try {
+        let response = await fetch(url);
+        let info = await response.json();
+        return info;
+    } catch (error) {
+        console.log(`E01: Failed to fetch ${url}`);
+        return;
+    }
+}
+
+function printData(info, args) {
+    for (const data in info.data) {
+        console.log('~~~~~~~~~~~~~~~~~~~~~~~~~\n');
+        for (const category in info.data[data]) {
+            if (args.has(category)) {
+                const categoryData = info.data[data][category];
+                if (!categoryData) {
+                    continue;
+                } else if (categoryData.constructor === Array) {
+                    printFromObjectList(category, categoryData);
+                } else if (categoryData.constructor === Object) {
+                    printFromObject(category, categoryData);
+                } else {
+                    printField(category, categoryData);
+                }
+                console.log();
+            }
+        }
+        console.log('~~~~~~~~~~~~~~~~~~~~~~~~~');
+    }
+}
+
+function printFromObjectList(categoryName, categoryObjList) {
+    const updatedCategoryName = correctName(categoryName);
+    console.log(attrName(`${updatedCategoryName}:`));
+    for (const categoryObj in categoryObjList) {
+        for (const attribute in categoryObjList[categoryObj]) {
+            process.stdout.write(`\t${categoryObjList[categoryObj][attribute]} `);
+        }
+        console.log();
+    }
+}
+
+function printFromObject(categoryName, categoryObj) {
+    const updatedCategoryName = correctName(categoryName);
+    console.log(attrName(`${updatedCategoryName}:`));
+    for (const attribute in categoryObj) {
+        process.stdout.write(`\t${attribute.slice(0, 5)} \t${categoryObj[attribute]}`);
+        console.log();
+    }
+}
+
+function printField(categoryName, categoryData) {
+    switch (categoryName) {
+        case "name":
+            console.log(name(categoryData));
+            break;
+        case "quote":
+            console.log(quote(`"${categoryData}"`));
+            break;
+        case "location":
+            console.log(`${attrName('Located @ ')}${categoryData}`);
+            break;
+        default:
+            const updatedCategoryName = correctName(categoryName);
+            console.log(`${attrName(`${updatedCategoryName}:`)} ${categoryData}`);
+            break;
+    }
+}
+
+function correctName(name) {
+    switch (name) {
+        case "attackPower":
+            return "Attack Power";
+        case "dmgNegation":
+            return "Damage Negation";
+        case "healthPoints":
+            return "HP";
+        case "defence":
+            return "Defense";
+        case "requiredAttributes":
+            return "Requirements";
+        case "scalesWith":
+            return "Scales With";
+        case "fpCost":
+            return "FP Cost";
+        case "hpCost":
+            return "HP Cost";
+        default:
+            return `${name.substring(0, 1).toUpperCase()}${name.substring(1)}`
+    }
 }
